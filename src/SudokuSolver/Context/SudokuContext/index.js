226135a02ae4c917168a2e75d54bf3037ab60d02
thanle95 <lengocthan95@gmail.com>
2023-03-12 23:54:28 +0700

    Revert "show overlay timer"
    This reverts commit 68335d8379dec11b2082f642a99b4beb02047143.

--- src/SudokuSolver/Context/SudokuContext/index.js
+++ src/SudokuSolver/Context/SudokuContext/index.js
@@ -1,4 +1,5 @@
 import React, { createContext, useContext, useState } from "react";
+import moment from "moment";
 import PropTypes from "prop-types";
 
 const SudokuContext = createContext({
@@ -6,21 +7,28 @@ const SudokuContext = createContext({
   setNumberSelected: () => {},
   gameArray: [],
   setGameArray: () => {},
+  timeGameStarted: moment(),
+  setTimeGameStarted: () => {},
   cellSelected: -1,
   setCellSelected: () => {},
   initArray: [],
   setInitArray: () => {},
   currentArray: [],
   setCurrentArray: () => {},
+  won: false,
+  setWon: () => {},
 });
 
 export const SudokuProvider = ({ children }) => {
   let [numberSelected, setNumberSelected] = useState("0");
   let [gameArray, setGameArray] = useState([]);
+  let [difficulty, setDifficulty] = useState("Easy");
+  let [timeGameStarted, setTimeGameStarted] = useState(moment());
   let [fastMode, setFastMode] = useState(false);
   let [cellSelected, setCellSelected] = useState(-1);
   let [initArray, setInitArray] = useState([]);
   let [currentArray, setCurrentArray] = useState([]);
+  let [won, setWon] = useState(false);
 
   return (
     <SudokuContext.Provider
@@ -29,6 +37,10 @@ export const SudokuProvider = ({ children }) => {
         setNumberSelected,
         gameArray,
         setGameArray,
+        difficulty,
+        setDifficulty,
+        timeGameStarted,
+        setTimeGameStarted,
         fastMode,
         setFastMode,
         cellSelected,
@@ -37,6 +49,8 @@ export const SudokuProvider = ({ children }) => {
         setInitArray,
         currentArray,
         setCurrentArray,
+        won,
+        setWon,
       }}
     >
       {children}
